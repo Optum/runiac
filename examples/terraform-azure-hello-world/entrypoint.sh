5812b8a541cac10a8b4308e448e514b8ecd56176
mg <mg@optum.com>
2021-02-10 13:33:34 -0600

    Update entrypoint.sh

--- examples/terraform-azure-hello-world/entrypoint.sh
+++ examples/terraform-azure-hello-world/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if az account get-access-token ; then
-  echo "already logged in..."
+if [[ -z "$ARM_CLIENT_ID" || -z "$ARM_CLIENT_SECRET" || -z "$ARM_TENANT_ID"  || -z "$ARM_SUBSCRIPTION_ID" ]]; then
+  if az account get-access-token ; then
+    echo "already logged in..."
+  else
+    az login;
+  fi
 else
-  az login;
+  az login --service-principal --username "$ARM_CLIENT_ID" --password "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID"
 fi
 
 runiac
